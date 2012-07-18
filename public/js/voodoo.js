98f486718d8d7a5092d84bf26395ceb06e8eebf2
Qiaoyong Zhong <solary.sh@gmail.com>
2012-07-18 20:39:04 +0800

    New feature: auto select file path on mouse click

---
+++ public/js/voodoo.js
@@ -0,0 +1,17 @@
+// Picked from http://jsfiddle.net/edelman/KcX6A/909/
+jQuery.fn.selectText = function(){
+    var doc = document;
+    var element = this[0];
+    console.log(this, element);
+    if (doc.body.createTextRange) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        var selection = window.getSelection();        
+        var range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
